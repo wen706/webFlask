bac453d4cbd8ac445cc79c9369d5ae5e6e4f588d
wen <wiwi30105@gmail.com>
2024-06-21 16:44:57 +0800

    自動安裝虛擬環境

--- run.cmd
+++ run.cmd
@@ -1,7 +1,19 @@
 @echo off
 rem 切換工作目錄至批次檔所在目錄
 cd /d "%~dp0"
-if not defined _OLD_VIRTUAL_PROMPT (
-	call env\Scripts\activate
+
+rem 檢查 env 資料夾是否存在
+if not exist "env" (
+    echo 建立虛擬環境...
+    python -m venv env
+
+    echo 激活虛擬環境並安裝依賴...
+    call env\Scripts\activate
+    pip install -r requirements.txt
 )
+
+if not defined _OLD_VIRTUAL_PROMPT (
+    call env\Scripts\activate
+)
+
 flask --debug -A app run -h 0.0.0.0 -p 80
